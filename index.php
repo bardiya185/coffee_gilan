e92244bd80aae7cf028fb9a15912c02e0b91f5c6
Bardiya vahedi <brdyawahdy243@gmail.com>
2025-10-24 23:06:14 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
+<?php include("layouts/header.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
+      <link rel="stylesheet" href="./styles/footer_style.css">
     <title></title>
 </head>
 <body>
@@ -40,3 +42,4 @@
     </div>
 </body>
 </html>
+<?php include("layouts/footer.php"); ?>
